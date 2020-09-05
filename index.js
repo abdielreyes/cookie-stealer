255cb9c4e748ccbcbe898245c2b75c25bb7122c5
Abdiel Reyes <abykings1@hotmail.com>
2020-09-05 16:12:52 -0500

    acepra usuario y pass

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8080
 app.get('/', (req, res) => {
 //	var cookie = req.query.cookie;
 // 	console.log('New cookie '+cookie);
-	var user = req.query.cookie;
+	var user = req.query.user;
 	var pass = req.query.pass;
 	console.log('user: '+user+' pass: '+pass);
   	res.send('Saludos a la bandita del CTF')
